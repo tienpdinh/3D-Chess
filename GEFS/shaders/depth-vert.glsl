2d8a736bddbb6debba7f460663231183a94fd0dd
Dan Shervheim <sherv029@umn.edu>
2019-10-15 19:32:48 -0500

    Cleaned up shadows code.

--- GEFS/shaders/depth-vert.glsl
+++ GEFS/shaders/depth-vert.glsl
@@ -1,15 +1,14 @@
 #version 330 core
 
-in vec3 position; //Changes for each vertex
-
+in vec3 position;  // Changes for each vertex.
 out vec3 pos;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 proj;
 
-
-void main() {
-  gl_Position = proj * view * model * vec4(position,1.0);
-  pos = (view * model * vec4(position,1.0)).xyz;
+void main()
+{
+    gl_Position = proj * view * model * vec4(position, 1.0);
+    pos = (view * model * vec4(position, 1.0)).xyz;
 }
--- engineCode/Shadows.h
+++ engineCode/Shadows.h
@@ -4,17 +4,17 @@
 #include "Materials.h"
 #include "Models.h"
 
-//Shadow mapping functions
+// Shadow mapping functions.
 void initShadowMapping();
 void initShadowBuffers();
 void computeShadowDepthMap(glm::mat4 lightView, glm::mat4 lightProjection, std::vector<Model*> toDraw);
 void computeShadowDepthMap(glm::mat4 lightView, glm::mat4 lightProjection, std::vector<Model*> toDrawShadows, float left, float right, float top, float bottom, float near, float far);
 void drawGeometryShadow(int shaderProgram, Model model, Material material, glm::mat4 transform);
 
-//Configuration values that can be set:
+// Configuration values that can be set.
 extern unsigned int shadowMapWidth, shadowMapHeight;
 
-//Global values we write out:
+// Global values we write out.
 extern unsigned int depthMapTex;
 
 #endif //SHADOWS_H
